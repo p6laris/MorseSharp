2f97a7de51fd5f92d07f52bc4edaf8b597bde2bb
Yunis Surchy <Yunis Surchy@YUNIS>
2022-09-27 18:38:29 +0300

    Add project files.

---
+++ MorseSharp/MorseCharacters.cs
@@ -0,0 +1,76 @@
+﻿namespace MorseSharp
+{
+    /// <summary>
+    /// Defines all morse characters.
+    /// </summary>
+    internal static class MorseCharacters
+    {
+        /// <summary>
+        /// Defines all morse characters in english language.
+        /// </summary>
+        /// <returns><see cref="System.Collections.Generic.Dictionary{TKey, TValue}"></see> of morse characters./></returns>
+        public static Dictionary<char, string> GetMorseCharactersEnglish()
+        {
+            return new Dictionary<char, string>()
+            {
+                { 'A', "._" },
+                { 'B', "_..." },
+                { 'C', "_._." },
+                { 'D', "_.."},
+                { 'E', "." },
+                { 'F', ".._." },
+                { 'G', "__." },
+                { 'H', "...." },
+                { 'I', ".." },
+                { 'J', ".___" },
+                { 'K', "_._" },
+                { 'L', "._.." },
+                { 'M', "__" },
+                { 'N', "_." },
+                { 'O', "___" },
+                { 'P', ".__." },
+                { 'Q', "__._" },
+                { 'R', "._." },
+                { 'S', "..." },
+                { 'T', "_" },
+                { 'U', ".._" },
+                { 'V', "..._" },
+                { 'W', ".__"},
+                { 'X', "_.._" },
+                { 'Y', "_.__" },
+                { 'Z', "__.." },
+                //Numerics
+                { '1', ".____" },
+                { '2', "..__" },
+                { '3', "...__" },
+                { '4', "...._" },
+                { '5', "....." },
+                { '6', "_...." },
+                { '7', "__..." },
+                { '8', "___.." },
+                { '9', "____." },
+                { '0', "_____" },
+                //Space
+                {' ', "/" },
+                //Punctuation
+                {'.',"._._._" },
+                {',',"__..__" },
+                {'?',"..__.." },
+                {';',"_._._." },
+                {':',"___..." },
+                {'/',"_.._."},
+                {'\'',".____." },
+                {'\"',"._.._." },
+                //Special Characters
+                {'_',"..__._"},
+                {'+',"._._." },
+                {'-',"_...._" },
+                {'*',"_.._" },
+                {'/',"___..." },
+                {'=',"_..._" },
+                {')',"_.__._" },
+                {'(',"_.__." }
+        };
+        }
+    }
+}
---
+++ MorseSharp/MorseConverter/MorseTextConverter.cs
@@ -0,0 +1,47 @@
+﻿using MorseSharp.MorseRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseSharp.MorseConverter
+{
+    public class MorseTextConverter : IMorseConverter
+    {
+        /// <summary>
+        /// <see cref="System.Text.StringBuilder"></see> object to append converted morse codes./>
+        /// </summary>
+        private StringBuilder? strBuilder;
+
+        /// <summary>
+        /// Loads the morse characters.
+        /// </summary>
+        private Lazy<Dictionary<char, string>> morse;
+
+        public MorseTextConverter()
+        {
+            morse = new Lazy<Dictionary<char, string>>(MorseCharacters.GetMorseCharactersEnglish);
+        }
+        public Task<string> ConvertToMorse(string text)
+        {
+            strBuilder = new StringBuilder();
+            text.ToUpper();
+
+            if(text is not null)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (morse.Value.ContainsKey(text[i]))
+                        strBuilder.Append(morse.Value[text[i]].AsSpan());
+                    else
+                        throw new Exception($"The {text[i]} character is not presented.");
+                }
+
+                return Task.Run(() => strBuilder.ToString());
+            }
+            
+            throw new ArgumentNullException(nameof(text));
+        }
+    }
+}
