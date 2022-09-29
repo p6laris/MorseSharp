026e3d0a603554143bf6d11d8b0d104e8d453200
Yunis surchy <Yunissurchy@gmail.com>
2022-09-29 15:20:41 +0300

    Console Example

---
+++ Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using MorseSharp;
+using MorseSharp.MorseConverter;
+
+MorseTextConverter converter = new MorseTextConverter();
+
+string morse = string.Empty;
+try
+{
+    morse = await converter.ConvertToMorse("Hello Morse");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine(morse);
