6df759a896c5cd13b145043200984e4b8008145f
p6laris <yunissurchy@gmail.com>
2023-11-15 03:40:15 +0300

    Added the Fluent API chain Interfaces.

---
+++ MorseSharp/Interfaces/ICanConvertToLight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseSharp.Interfaces
+{
+    public interface ICanConvertToLight
+    {
+        Task DoBlinks(Action<bool> blinkerAction);
+    }
+}
---
+++ MorseSharp/Interfaces/ICanGenerateAudioAndLight.cs
@@ -0,0 +1,17 @@
+﻿namespace MorseSharp.Interfaces
+{
+    /// <summary>
+    /// Represents an interface that allows specifying audio options for Morse code conversion.
+    /// </summary>
+    public interface ICanSetAudioOptions
+    {
+        /// <summary>
+        /// Sets audio options for Morse code conversion.
+        /// </summary>
+        /// <param name="charSpeed">The character speed in words per minute (WPM).</param>
+        /// <param name="wordSpeed">The word speed in words per minute (WPM).</param>
+        /// <param name="frequency">The frequency (in Hertz) of the audio signal.</param>
+        /// <returns>An object that allows further conversion to audio with the specified options.</returns>
+        ICanConvertToAudio SetAudioOptions(int charSpeed, int wordSpeed, double frequency);
+    }
+}
