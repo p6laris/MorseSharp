d82e86df63671043cf6500628b1061a9378d329b
p6laris <yunissurchy@gmail.com>
2023-11-15 03:33:09 +0300

    Added the exceptions.

---
+++ MorseSharp/Exceptions/SmallerWordSpeedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseSharp.Exceptions
+{
+    [Serializable]
+    internal class SmallerWordSpeedException : Exception
+    {
+        public SmallerWordSpeedException() { }
+
+        public SmallerWordSpeedException(string message) : base(message)
+        {
+
+        }
+        public SmallerWordSpeedException(int charSpeed, int wordSpeed)
+            : base($"The character speed must not be smaller than word speed: {charSpeed} < {wordSpeed}")
+        {
+
+        }
+    }
+}
---
+++ MorseSharp/Exceptions/WordNotPresentedException.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MorseSharp.Exceptions
+{
+    [Serializable]
+    internal class WordNotPresentedException : Exception
+    {
+
+        public WordNotPresentedException() { }
+        public WordNotPresentedException(char word)
+            : base($"The {word} word is not presented in the specified Language.")
+        {
+
+        }
+        public WordNotPresentedException(char word, Language language)
+            : base($"The {word} word is not presented in {Enum.GetName(language)} Language.")
+        {
+
+        }
+    }
+}
