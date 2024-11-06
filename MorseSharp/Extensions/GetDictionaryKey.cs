7ed3360d374b409901b82a56d79319df23bf1734
p6laris <yunissurchy@gmail.com>
2024-11-06 06:53:28 +0300

    Simplify and deleting the generics

---
+++ MorseSharp/Extensions/GetDictionaryKey.cs
@@ -0,0 +1,21 @@
+﻿namespace MorseSharp.Extensions;
+
+
+internal static class GetDictionaryKey
+{
+    public static char KeyByValue(this Dictionary<char, string> dict, string val)
+    {
+        if (string.IsNullOrEmpty(val))
+            return '\0'; 
+
+        foreach (KeyValuePair<char, string> pair in dict)
+        {
+            if (pair.Value == val)
+            {
+                return pair.Key;  
+            }
+        }
+
+        return '\0';  
+    }
+}
