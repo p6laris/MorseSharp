581f7e417e0c299dc40ee1fd0b846dac8d8e7363
p6laris <yunissurchy@gmail.com>
2024-11-13 06:05:46 +0300

    Accidentally added the benchmark

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,46 +1,35 @@
 ﻿using MorseSharp;
 using BenchmarkDotNet;
 using BenchmarkDotNet.Running;
-using ConsoleExample;
 
+try
+{
+    //Encoding
+    var morse = Morse.GetConverter()
+        .ForLanguage(Language.English)
+        .ToMorse("Hi")
+        .Encode();
 
-//Morse.GetConverter()
-//    .ForLanguage(Language.English)
-//    .ToAudio(".... ..")
-//    .SetAudioOptions(25, 25, 600)
-//    .GetBytes(out var _);
+    //Decoding
+    var text = Morse.GetConverter()
+        .ForLanguage(Language.English)
+        .Decode(".... ..");
 
+    //Light Blinking
+    await Morse.GetConverter()
+        .ForLanguage(Language.English)
+        .ToLight(".... ..")
+        .SetBlinkerOptions(25, 25)
+        .DoBlinks((hasToBlink) =>
+        {
+            if (hasToBlink)
+                Console.BackgroundColor = ConsoleColor.White;
+            else
+                Console.BackgroundColor = ConsoleColor.Black;
+        });
 
-var summery = BenchmarkRunner.Run<MorseBenchmark>();
-
-//try
-//{
-//    //Encoding
-//    var morse = Morse.GetConverter()
-//        .ForLanguage(Language.English)
-//        .ToMorse("Hi")
-//        .Encode();
-
-//    //Decoding
-//    var text = Morse.GetConverter()
-//        .ForLanguage(Language.English)
-//        .Decode(".... ..");
-
-//    //Light Blinking
-//    await Morse.GetConverter()
-//        .ForLanguage(Language.English)
-//        .ToLight(".... ..")
-//        .SetBlinkerOptions(25, 25)
-//        .DoBlinks((hasToBlink) =>
-//        {
-//            if (hasToBlink)
-//                Console.BackgroundColor = ConsoleColor.White;
-//            else
-//                Console.BackgroundColor = ConsoleColor.Black;
-//        });
-
-//}
-//catch (Exception ex)
-//{
-//    Console.WriteLine(ex.Message);
-//}
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
